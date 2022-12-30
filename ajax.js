2b7d2ebcbb5eca9a070a612ae2cac3c4079a60e2
yaocaptain <goof051505@gmail.com>
2022-12-31 03:44:44 +0800

    大改css

--- ajax.js
+++ ajax.js
@@ -3,9 +3,8 @@
 // var dataUrl= "x.json"
 var dataUrl= "https://raw.githubusercontent.com/yaocaptain/mj/main/data_dict.json"  // 公開 data_dict.json 65MB
 var d  // data_dict
-var id  // 用 hot_score 排序過的
+var id  // 存每張圖的id
 var loaded_count = 0;  // 定義一個變數，記錄已經載入的圖片數量
-console.log(dataUrl);
 $.ajax({
     url: dataUrl,
     method: 'GET',
@@ -15,7 +14,8 @@ $.ajax({
 
     success: res =>{
         d = res
-        id = Object.keys(d).sort((a, b) => d[b]['hot_score'] - d[a]['hot_score']);  // id[0] 的 'hot_score' 最高
+        // id = Object.keys(d).sort((a, b) => d[b]['hot_score'] - d[a]['hot_score']);  // id[0] 的 'hot_score' 最高
+        id = Object.keys(d).sort(()=> Math.random() - 0.5);  // 隨機打亂
         console.log('ajax 加載成功')
         console.log('typeof(res)', typeof(res));
         console.log('Object.keys(d).length:', Object.keys(d).length)
@@ -40,10 +40,9 @@ function checkScroll() {
     var clientHeight = document.body.clientHeight;
     // 如果滾動條的高度加上瀏覽器視窗的高度等於網頁的總高度，表示已滑到底部
     // if (scrollTop + clientHeight === scrollHeight) {
-    if (scrollTop + clientHeight >= scrollHeight - 1000) {
+    if (scrollTop + clientHeight >= scrollHeight - 1600) {
         // console.log('滑到底部了');
-        // // 載入 20 張圖片
-        loadImages();
+        loadImages();  // 載入 20 張圖片
     } else {
         // console.log("還沒到底");
     }
@@ -68,26 +67,30 @@ function loadImages() {
         var div = document.getElementById(`id${i % 4}`);
         var imgContainer = document.createElement('div');
         imgContainer.className = 'divhere'
+        // var img384 = document.createElement('img');
         var img = document.createElement('img');
+        // img384.className = 'img384'
         img.className = 'img'
         var btnContainer = document.createElement('div');
         btnContainer.className = 'btnContainer'
         btnContainer.style.display = 'none';
         // img.src = `https://cdn.midjourney.com/${id[i]}/0_0.png`;  # 最高清圖
-        img.src = `https://cdn.midjourney.com/${id[i]}/grid_0_384_N.webp`;
+        // img384.src = `https://cdn.midjourney.com/${id[i]}/grid_0_384_N.webp`;
+        img.src = `https://cdn.midjourney.com/${id[i]}/grid_0_640_N.webp`;
         // https://cdn.midjourney.com/6e061cb6-02f1-4969-ab47-8399c1748705/grid_0_640_N.webp  # 大
         // https://cdn.midjourney.com/6e061cb6-02f1-4969-ab47-8399c1748705/grid_0_384_N.webp  # 中
         // https://cdn.midjourney.com/6e061cb6-02f1-4969-ab47-8399c1748705/grid_0_128_N.webp  # 小
 
-        img.style.width = '384px';
-        img.style.marginBottom = `${400-384}px`;
+        // img.style.width = '384px';
+        // img384.style.marginBottom = `${400-384}px`;
+        // img.style.marginBottom = `${400-384}px`;
+        // img384.title = `${d[id[i]]['prompt']}`
         img.title = `${d[id[i]]['prompt']}`
 
         // downloadButton
         var downloadButton = document.createElement('button');
         downloadButton.innerHTML = '原始圖片';
         downloadButton.dataset.index = i;
-        // downloadButton.className = 'downloadButton'
         downloadButton.className = 'greenCss'
         let pic_url = `${d[id[i]]['image_paths']}`
         downloadButton.onclick = function(){
@@ -100,7 +103,6 @@ function loadImages() {
         // copyTextButton
         var copyTextButton = document.createElement('button');
         copyTextButton.innerHTML = '複製prompt';
-        // copyTextButton.className = 'copyTextButton'
         copyTextButton.className = 'greenCss'
 
         let full_command = `${d[id[i]]['full_command']}`
@@ -150,6 +152,7 @@ function loadImages() {
         }
         btnContainer.appendChild(copyTextButton);
         imgContainer.appendChild(btnContainer);
+        // imgContainer.appendChild(img384);
         imgContainer.appendChild(img);
         div.appendChild(imgContainer);
     }
